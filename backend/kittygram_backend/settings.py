45374aa5a76b6a35206cc01a25ae36bd3351b081
ipoderator <glebchurkin48@gmail.com>
2023-12-30 00:27:27 +0300

    newer

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,13 +1,14 @@
 # flake8: noqa
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django')
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret')
 
 DEBUG = False
 
