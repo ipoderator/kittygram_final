3a43c4586b48f40fdee848c9b9d2e48549718375
Gleb <89800956+ipoderator@users.noreply.github.com>
2024-01-02 14:05:26 +0300

    wpw

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
-
+"""wow"""
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret')
